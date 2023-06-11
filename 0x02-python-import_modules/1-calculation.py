234592667bb0fa730244008f7ac72a97c395b1c9
shem <shemomondi746@gmail.com>
2023-06-11 14:36:00 -0700

    corrections and updates on this number

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,23 +1,14 @@
-def perform_calculations():
-    """Perform the sum, difference, multiplication, and division of 10 and 5."""
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    sum_result = add(a, b)
-    difference_result = sub(a, b)
-    multiplication_result = mul(a, b)
-    quotient_result = div(a, b)
-
-    return sum_result, difference_result, multiplication_result, quotient_result
-
-
-if __name__ == "__main__":
-    sum_result, difference_result, multiplication_result, quotient_result = perform_calculations()
-
-    print("{} + {} = {}".format(10, 5, sum_result))
-    print("{} - {} = {}".format(10, 5, difference_result))
-    print("{} * {} = {}".format(10, 5, multiplication_result))
-    print("{} / {} = {}".format(10, 5, quotient_result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
