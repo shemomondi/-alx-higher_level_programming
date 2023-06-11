6754b7ea37afe99ac7e6009e0736ab4ac62db079
shem <shemomondi746@gmail.com>
2023-06-11 13:33:17 -0700

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 5-no_c.py
+
+
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            new_string += char
+    return new_string
+
