8e19f3f72c19276d2f19c2c4f3f4490507418060
shem <shemomondi746@gmail.com>
2023-06-11 14:34:04 -0700

    corrections and updates

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python3
+def perform_calculations():
+    """Perform the sum, difference, multiplication, and division of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    sum_result = add(a, b)
+    difference_result = sub(a, b)
+    multiplication_result = mul(a, b)
+    quotient_result = div(a, b)
+
+    return sum_result, difference_result, multiplication_result, quotient_result
+
 
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
-   from calculator_1 import add, sub, mul, div
+    sum_result, difference_result, multiplication_result, quotient_result = perform_calculations()
 
-a = 10
-b = 5
+    print("{} + {} = {}".format(10, 5, sum_result))
+    print("{} - {} = {}".format(10, 5, difference_result))
+    print("{} * {} = {}".format(10, 5, multiplication_result))
+    print("{} / {} = {}".format(10, 5, quotient_result))
 
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
-
-print("The addition result is:", add_result)
-print("The subtraction result is:", sub_result)
-print("The multiplication result is:", mul_result)
-print("The division result is:", div_result)
