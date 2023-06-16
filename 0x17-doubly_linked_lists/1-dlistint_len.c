d95d28af369975f102949c0b6b273745222d318c
shem <shemomondi746@gmail.com>
2023-06-16 11:57:34 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,20 @@
 #include <stddef.h>
-#include "lists.h"
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
 size_t dlistint_len(const dlistint_t *h)
 {
