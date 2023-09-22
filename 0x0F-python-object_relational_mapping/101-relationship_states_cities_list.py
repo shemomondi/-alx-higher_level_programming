a38bf05a742016bf1719fe8d2132ab7ac0935a0d
shem <shemomondi746@gmail.com>
2023-09-22 12:01:23 -0700

    Making some few corrections and error handling

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa"""
+'''Prints all State objects and their City objects in a database.
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
+from relationship_state import Base, State
+from relationship_city import City
 
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-    session.close()
-
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).order_by(State.id.asc()).all()
+        for state in result:
+            print('{}: {}'.format(state.id, state.name))
+            for city in state.cities:
+                print('\t{}: {}'.format(city.id, city.name))
+        session.close()
